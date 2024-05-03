c11b11757120123b207f0b6b5b152105c486dfd4
laizhenyoong <150910325+laizhenyoong@users.noreply.github.com>
2024-05-03 17:41:15 +0800

    Update main.py

--- src/pipeline/main.py
+++ src/pipeline/main.py
@@ -1,8 +1,19 @@
 #!/usr/bin/env python3
 
+import httpx
+from prefect import flow, get_run_logger
+
+@flow 
+def collect_petstore_inventory():
+    logger = get_run_logger()
+    url = "https://petstore.swagger.io/v2/store/inventory"
+    response = httpx.get(url)
+    response.raise_for_status()
+    inventory_stats = response.json()
+    logger.info(inventory_stats)
 
 def main():
-    print("hello world")
+    collect_petstore_inventory.serve("petstore-collection-deployment")
 
 
 if __name__ == "__main__":
