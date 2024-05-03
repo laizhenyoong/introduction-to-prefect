ccee74bd44b4c7342bdaac1ad612dc017fec66e6
laizhenyoong <150910325+laizhenyoong@users.noreply.github.com>
2024-05-03 18:38:02 +0800

    Updated main.py with task and flow functions

--- src/pipeline/main.py
+++ src/pipeline/main.py
@@ -1,16 +1,40 @@
 #!/usr/bin/env python3
 
 import httpx
-from prefect import flow, get_run_logger
+from prefect import flow, get_run_logger, task
 
-@flow 
-def collect_petstore_inventory():
+@task 
+def retrieve_from_api(
+        base_url,
+        path,
+        secure
+    ):
     logger = get_run_logger()
-    url = "https://petstore.swagger.io/v2/store/inventory"
+    if secure:
+        url = f"https://{base_url}{path}"
+    else: 
+        url = f"http://{base_url}{path}"
     response = httpx.get(url)
     response.raise_for_status()
     inventory_stats = response.json()
     logger.info(inventory_stats)
+
+@task 
+def clean_state_data(inventory_stats):
+    return {
+        "sold": inventory_stats.get("sold", 0) + inventory_stats.get("Sold", 0),
+        "available": inventory_stats.get("available", 0) + inventory_stats.get("Available", 0),
+        "unavailable": inventory_stats.get("unavailable", 0) + inventory_stats.get("Unavailable", 0),
+        "pending": inventory_stats.get("pending", 0) + inventory_stats.get("Pending", 0)
+    }
+
+@flow 
+def collect_petstore_inventory(
+        base_url: str="petstore.swagger.io",
+        path: str="/v2/store/inventory",
+        secure: bool=True
+    ):
+    inventory_stats = retrieve_from_api(base_url, path, secure)
 
 def main():
     collect_petstore_inventory.serve("petstore-collection-deployment")
